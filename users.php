934117e1952de6e6259ee5ccab88d2c7a97796c4
matteopoliti <matteo.politi17@gmail.com>
2024-06-27 12:19:06 +0200

    Feat: added users list page

---
+++ users.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MattChat</title>
+
+    <!-- Link font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Link Css -->
+    <link rel="stylesheet" href="./styles/general.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="users">
+            <header>
+                <div class="content">
+                    <img src="./assets/Pro-pic.jpg" alt="Profile photo">
+                    <div class="details">
+                        <span>Matteo</span>
+                        <p>Attivo Adesso</p>
+                    </div>
+                </div>
+                <a href="#" class="logout">Logout</a>
+            </header>
+            <div class="search">
+                <span class="text">Selezione un utente per iniziare la chat</span>
+                <input type="text" name="" id="" placeholder="Inserisci il nome per cercare...">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="users-list">
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="./assets/Pro-pic.jpg" alt="">
+                        <div class="details">
+                            <span>Matteo</span>
+                            <p>Attivo Adesso</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
