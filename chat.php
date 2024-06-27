295c07fd97fa67f31518d8cd99152ae6f0e5adf8
matteopoliti <matteo.politi17@gmail.com>
2024-06-27 16:25:49 +0200

    Feat: added chat page

---
+++ chat.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MattChat</title>
+
+    <!-- Link font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Link Css -->
+    <link rel="stylesheet" href="./styles/general.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="./assets/Pro-pic.jpg" alt="Profile photo">
+                <div class="details">
+                    <span>Matteo</span>
+                    <p>Attivo Adesso</p>
+                </div>
+
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="./assets/Pro-pic.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="./assets/Pro-pic.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="./assets/Pro-pic.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="./assets/Pro-pic.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <form action="" class="typing-area">
+                <input type="text" placeholder="Scrivi un messaggio qui...">
+                <button><i class="fas fa-paper-plane"></i></button>
+            </form>
+        </section>
+    </div>
+</body>
+
+</html>
